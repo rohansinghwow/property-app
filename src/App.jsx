826e768fe449949bb5c8a07ca31b8a8023ba615e
Rohan Singh <rohan.singh@codemonk.in>
2022-11-30 13:58:56 +0530

    setup home

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-
+import { Home } from "./Home";
 export default () => (
   <>
-    <h1 className="">Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
+    <Home />
   </>
 );
